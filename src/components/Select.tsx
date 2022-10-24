56e18feeeec933b9eb8c48d82d90fc8ffe6e4f87
Tait Larson <telarson@gmail.com>
2022-10-23 20:27:37 -0400

    Select done. About to try to use generics for stories.

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,22 +1,118 @@
 import React, { useState } from "react";
 import { Listbox } from "@headlessui/react";
+import classNames from "classnames";
+import { FiCheck, FiChevronDown } from "react-icons/fi";
 
+// I could have imported this. Decided not to. Hoping to make it a generic.
+export interface IOption {
+  value: string;
+  label: string | React.ReactNode;
+}
+
+export interface SelectProps {
+  options: IOption[];
+  selectedOption: IOption | undefined;
+  setSelectedOption: (option: string) => void;
+  placeholder?: string;
+  LeadingIcon?: React.ReactElement;
+  width?: string;
+}
+
+export const Select = ({
+  options,
+  selectedOption,
+  setSelectedOption,
+  placeholder,
+  LeadingIcon,
+  width,
+}: SelectProps) => {
+  return (
+    <div className={classNames("relative inline-block", width)}>
+      <Listbox
+        value={selectedOption && selectedOption.value}
+        onChange={setSelectedOption}
+      >
+        {({ open }) => (
+          <>
+            <Listbox.Button
+              className={classNames(
+                "select-button",
+                {
+                  "select-selected": selectedOption,
+                  "select-not-selected": !selectedOption,
+                },
+                width,
+              )}
+            >
+              {LeadingIcon ? (
+                <div className="w-5 h-5 mr-2 overflow-hidden">
+                  <LeadingIcon.type size={20} className="text-gray-400" />
+                </div>
+              ) : null}
+              {selectedOption ? selectedOption : placeholder}
+              <FiChevronDown
+                size={20}
+                className={classNames(
+                  "text-gray-50 dark:text-gray-300 transform duration-100 ease-out",
+                  {
+                    "-rotate-180": open,
+                    "ml-auto": width,
+                    "ml-3.5": width,
+                  },
+                )}
+              />
+            </Listbox.Button>
+            <Listbox.Options className="select-options">
+              {options.map((option, index) => {
+                <Listbox.Option
+                  as={React.Fragment}
+                  key={option.value}
+                  value={option.value}
+                >
+                  {({ active, selected }) => (
+                    <li
+                      className={classNames("selected-option", {
+                        "bg-primary-25 dark:bg-gray-100 dark:bg-opacity-10":
+                          active,
+                        "rounded-t-lg": index === 0,
+                        "rounded-b-lg": index === options.length - 1,
+                      })}
+                    >
+                      <div className="flex items-center">{option.label}</div>
+                      {selected ? (
+                        <FiCheck className="ml-5 text-primary-600 dark:text-white" />
+                      ) : null}
+                    </li>
+                  )}
+                </Listbox.Option>;
+              })}
+            </Listbox.Options>
+          </>
+        )}
+      </Listbox>
+    </div>
+  );
+};
+
+// -------------------------------------------------------------
+
+// --- mine ---
 export interface ISelectOption<T> {
   id: T;
   value: React.ReactNode;
 }
 
-export interface SelectProps<T> {
+export interface ISelectProps<T> {
   options: ISelectOption<T>[];
   initial: T | null;
   emptyButton: React.ReactNode;
 }
 
-export const Select = <T extends unknown>({
+export const SelectV1 = <T extends unknown>({
   options,
   initial,
   emptyButton,
-}: SelectProps<T>) => {
+}: ISelectProps<T>) => {
   const [selected, setSelected] = useState<T | null>(initial);
 
   return (
--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Meta, Story } from "@storybook/react";
 import { StoryLayout } from "../components/StoryLayout";
-import { Figma } from "../data";
-import { Select, SelectProps, ISelectOption } from "../components/Select";
+import { SelectV1, ISelectProps, ISelectOption } from "../components/Select";
 import { FiDollarSign } from "react-icons/fi";
+import { countries, dates, prices, Figma } from "../data";
 
 const meta: Meta = {
   title: "Select",
-  component: Select,
+  component: SelectV1,
   parameters: {
     controls: {
       expanded: true,
@@ -21,7 +21,7 @@ const meta: Meta = {
 
 export default meta;
 
-interface Props extends SelectProps<string> {
+interface Props extends ISelectProps<string> {
   darkMode: boolean;
 }
 
@@ -40,11 +40,27 @@ const StorySelect: Story<Props> = (args) => {
   // should the component manage the state?
   // does this only work if
 
+  const [selectedCountry, setSelectedCountry] = useState<IOption>(countries[0]);
+  const [selectedDate, setSelectedDate] = useState<IOption>();
+  const [selectedPrice, setSelectedPrice] = useState<IOption>();
+
   return (
-    <StoryLayout {...args} noPadding={false}>
-      <div>
-        <Select {...args}></Select>
-      </div>
+    <StoryLayout darkMode={args.darkMode} className={"space-x-3 space-y-3"}>
+      <SelectV1
+        options={countries}
+        selectedOption={selectedCountry}
+        setSelectedOption={setSelectedCountry}
+        placeholder="Select a Country"
+        width="w-50"
+      />
+      <SelectV1
+        options={prices}
+        LeadingIcon={<FiDollarSign />}
+        selectedOption={selectedDate}
+        setSelectedOption={setSelectedDate}
+        placeholder="Select a Date"
+        width="w-50"
+      />
     </StoryLayout>
   );
 };
